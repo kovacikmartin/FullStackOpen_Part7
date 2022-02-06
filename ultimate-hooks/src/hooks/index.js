290b9b17ea194f6d0674eeebb560c0376535f3ea
Martin Kováčik <martn.kovacik@gmail.com>
2022-02-06 21:18:32 +0100

    exercise 7.8

---
+++ ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useResource = (baseUrl) => {
+
+    const [resources, setResources] = useState([])
+
+
+    useEffect(() => {
+
+        const getAll = async () => {
+
+            const response = await axios.get(baseUrl)    
+            setResources(response.data)
+        }
+        getAll()
+
+    }, [baseUrl])
+    
+    const create = async (resource) => {
+
+        const response = await axios.post(baseUrl, resource)
+
+        setResources(resources.concat(response.data))
+    }
+    
+    const service = {
+        create
+    }
+    
+    return [
+        resources, service
+    ] 
+}
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+  
+    const onChange = (event) => {
+      setValue(event.target.value)
+    }
+  
+    return {
+      type,
+      value,
+      onChange
+    }
+}
